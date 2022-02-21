81677a30897b9be8eb5eea1a6971fb5fe30feccd
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2022-02-21 11:57:11 -0600

    Se agregaron sensores ambientales

--- MIsSensores2022A/app/src/main/java/net/ivanvega/missensores2022a/MainActivity.java
+++ MIsSensores2022A/app/src/main/java/net/ivanvega/missensores2022a/MainActivity.java
@@ -4,20 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+ implements  SensorEventListener{
     SensorManager sensorManager;
+    TextView txtProximi, txtLuz;
 
+
+    Sensor sensorProximidad, sensorLuz;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtProximi = findViewById(R.id.txtProximidad);
+        txtLuz = findViewById(R.id.txtLuz);
 
         sensorManager = (SensorManager)
                 getSystemService(Context.SENSOR_SERVICE);
@@ -32,6 +41,64 @@ public class MainActivity extends AppCompatActivity {
             Log.d("SENSORX", sensor.toString());
 
         }
+
+        sensorProximidad =
+                sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        sensorLuz = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(sensorProximidad!=null) {
+            sensorManager.registerListener(sensorEventListenerAmbiental,
+                    sensorProximidad, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        if(sensorLuz!=null){
+            sensorManager.registerListener(sensorEventListenerAmbiental,
+                    sensorLuz, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(sensorEventListenerAmbiental);
+    }
+
+    SensorEventListener sensorEventListenerAmbiental =
+            new SensorEventListener() {
+                @Override
+                public void onSensorChanged(SensorEvent sensorEvent) {
+
+                    switch (sensorEvent.sensor.getType()){
+                        case Sensor.TYPE_PROXIMITY:
+                            txtProximi.setText(txtProximi.getText().toString() + ": " + sensorEvent.values[0]);
+                            break;
+                        case Sensor.TYPE_LIGHT:
+                            txtLuz.setText("Sensor de Luz" + ": " + sensorEvent.values[0]);
+                            break;
+                    }
+
+
+                }
+
+                @Override
+                public void onAccuracyChanged(Sensor sensor, int i) {
+
+                }
+            };
+
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
 
     }
 }
