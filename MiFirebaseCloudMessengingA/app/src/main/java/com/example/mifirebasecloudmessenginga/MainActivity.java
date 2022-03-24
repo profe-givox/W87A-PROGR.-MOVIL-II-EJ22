4b9bf0276eebc8e8e518e4b38b7c454a17b79102
profe-givox <gi.vega@itsur.edu.mx>
2022-03-24 11:21:28 -0600

    SE AGREGO EL PROYECTO DE NOTIFICACIONES PUSH CON FIREBASE. SE OBTIENE EL TOKEN Y RECIBE NOTIFICAICONES LANZADAS DESDE FIREBASE CLOUD MESSENGING

---
+++ MiFirebaseCloudMessengingA/app/src/main/java/com/example/mifirebasecloudmessenginga/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.mifirebasecloudmessenginga;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "GIVO Token FCM";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recuperarToken();
+    }
+
+    private void recuperarToken() {
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "Fetching FCM registration token failed", task.getException());
+                            return;
+                        }
+
+                        // Get new FCM registration token
+                        String token = task.getResult();
+
+                        // Log and toast
+
+                        Log.d(TAG, token);
+                        Toast.makeText(MainActivity.this, token, Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
+}
