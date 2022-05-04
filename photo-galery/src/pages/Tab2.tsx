8a331d88b9f393ee72fe1545ed9d9c34f4ed796d
Gustavo Ivan Vega Olvera <isc.givox@gmail.com>
2022-05-04 10:10:33 -0500

    SE AGREGO LA FUNCIONALIDAD DE CAMARA Y MOSAR LAS FOTOS TOMASS POR EL MOMENTO PARA WEB

--- photo-galery/src/pages/Tab2.tsx
+++ photo-galery/src/pages/Tab2.tsx
@@ -4,7 +4,11 @@ import { camera } from 'ionicons/icons';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
 
+import { usePhotoGallery } from '../hooks/usePhotoGallery';
+
 const Tab2: React.FC = () => {
+  const { photos, takePhoto } = usePhotoGallery();
+
   return (
     <IonPage>
       <IonHeader>
@@ -13,6 +17,16 @@ const Tab2: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
+        <IonGrid>
+          <IonRow>
+            {photos.map((photo, index) => (
+              <IonCol size="6" key={index}>
+                <IonImg src={photo.webviewPath} />
+              </IonCol>
+            ))}
+          </IonRow>
+        </IonGrid>
+
         <IonFab vertical='bottom' horizontal='center' slot='fixed'>
           <IonFabButton onClick={() => takePhoto()}>
             <IonIcon icon={camera}></IonIcon>
